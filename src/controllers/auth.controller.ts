a91ca56ed5801cc3c6c6d9c73bcaf051e4be73bc
Diego <dl7designer@gmail.com>
2023-10-13 20:36:50 -0300

    incluido geracao de token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,30 +1,38 @@
 import { Request, Response } from 'express'
 import userService from '../services/user.service'
 import { ResponseDto } from '../dto/response.dto'
-
+import { v4 as createToken } from 'uuid'
 class AuthController {
     public async login(req: Request, res: Response) {
+        const { username, email, password } = req.body
 
-        const { name, username, email, password } = req.body
-        const userLogged = await userService.getByUser(name, username, email, password)
+        const user = await userService.getByUser(
+            username,
+            email,
+            password
+        )
 
-        if (!userLogged) {
-            return res.status(401).send({ message: "username ou senha incorretos" })
+        if (!user) {
+            return res.status(401).send({ message: "Usuario ou senha incorretos" })
         }
 
-        const loggedModel = {
-            id: userLogged.id,
-            username: userLogged.username,
-            email: userLogged.email
-        }
+        const token = createToken()
+        const update = await userService.updateUser({ ...user, token: token })
+        const logged = userService.mapToModel(user).detailUser()
 
-        const response: ResponseDto = {
+        const result: ResponseDto = {
             ok: true,
             code: 200,
             message: "Login efetuado com sucesso",
-            data: loggedModel
+            data: {
+                logged: logged,
+                token: token
+            }
         }
-        return response
+
+        if (update.code === 200) {
+            return res.status(result.code).send(result)
+        }
     }
 }
 
