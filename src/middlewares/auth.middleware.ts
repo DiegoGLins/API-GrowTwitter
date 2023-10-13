160d626bbe6967f90c08b564517a441be63102cc
Diego <dl7designer@gmail.com>
2023-10-13 20:39:33 -0300

    alteracao do authmiddleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,16 +1,28 @@
 import { NextFunction, Request, Response } from "express";
+import userService from "../services/user.service";
 
 async function authMiddleware(req: Request, res: Response, next: NextFunction) {
     try {
-        const { username, email, paswword } = req.body
+        const token = req.headers.authorization
 
-        if (!username || !email || !paswword) {
+        if (!token) {
             return res.status(401).send({
-                ok: false,
                 code: 401,
-                message: "Autenticação do login falhou"
+                message: "Autenticação do token falhou"
             })
         }
+
+        const user = await userService.getByToken(token)
+
+        if (!user) {
+            return res.status(401).send({
+                code: 401,
+                message: "Autenticação do token falhou"
+            })
+        }
+
+        req.body.userID = user.id
+
         next()
     } catch (error: any) {
         return res.status(500).send({
