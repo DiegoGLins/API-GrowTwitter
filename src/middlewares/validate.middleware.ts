4ac0cd71a8fd57ad35a68cd2f46a5cbcc783bc19
Diego <dl7designer@gmail.com>
2023-10-13 20:40:04 -0300

    alteracao do validateMiddleware

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -2,13 +2,13 @@ import { NextFunction, Request, Response } from "express";
 
 async function validateUserMiddleware(req: Request, res: Response, next: NextFunction) {
 
-    const { name, username, email, password } = req.body
+    const { username, email, password } = req.body
 
-    if (!name || !username || !email || !password) {
+    if (!username || !email || !password) {
         return res.status(400).send({
             ok: false,
             code: 400,
-            message: "Name, username, email ou password não fornecidos"
+            message: "Username, email ou password não fornecidos"
         })
     }
     next()
