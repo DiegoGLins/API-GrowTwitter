7b474a1b33da50f19cbfe7ae4cf3a41dbc610778
Diego <dl7designer@gmail.com>
2023-10-13 20:41:37 -0300

    alteracao da rota de user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express"
 import { UserController } from "../controllers/user.controller"
+import validateUserMiddleware from "../middlewares/validate.middleware"
+import authMiddleware from "../middlewares/auth.middleware"
 
 export const userRoutes = () => {
     const router = Router()
     const controller = new UserController()
 
-    router.post("/", controller.register)
+    router.post("/", validateUserMiddleware, controller.register)
     router.get("/", controller.list)
-    router.delete("/:id", controller.delete)
-    router.put("/:id", controller.update)
+    router.delete("/:id", authMiddleware, controller.delete)
+    router.put("/:id", authMiddleware, controller.update)
 
     return router
 }
