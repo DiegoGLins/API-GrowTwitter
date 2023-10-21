550bec657dff3ad72779b5aa80691fab686ce978
Diego <dl7designer@gmail.com>
2023-10-21 15:01:07 -0300

    refatoracao da model tweet

--- src/models/tweet.model.ts
+++ src/models/tweet.model.ts
@@ -19,27 +19,12 @@ class Tweet {
         return this._id;
     }
 
-    //----- Inclui e remove e likes ------
-
-    public includeLike(tweet: Liker) {
-        this.likes.push(tweet)
-    }
-
-    public removeLike(like: Liker) {
-        const index = this.likes.findIndex(item => item.idLike === like.idLike)
-        if (index !== -1) {
-            this.likes.splice(index, 1)
-        }
-    }
-
     detailTweet() {
         return {
             idTweet: this._id,
             content: this.content,
             idUser: this.idUser,
             authorTweet: this.authorTweet,
-            likes: this.likes.length,
-            reTweet: this.reTweets.length
         }
     }
 
@@ -51,76 +36,6 @@ class Tweet {
             authorTweet: this.authorTweet
         }
     }
-
-    // // public feedTweet(tweet: string, user: Follow) {
-    // //     const findTweet = tweets.find(item => item.content === tweet)
-    // //     const findReply = replies.find(item => item.authorReply.id === user.id)
-
-
-    // //     if (findTweet) {
-    // //         if (findTweet?.likes.length === 0) {
-    // //             console.log(`@${user.username} : ${findTweet?.content}`)
-    // //             console.log(`[ ${findTweet.likes.length} curtidas ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-
-    // //         if (findTweet!.likes.length === 1) {
-    // //             console.log(`@${findTweet?.detailTweet().author} : ${findTweet?.content}`)
-    // //             console.log(`[ @${findTweet!.likes.map(item => item.authorlike.username)} curtiu isso ]`)
-    // //             console.log("=========================================================================")
-
-    // //         }
-
-    // //         if (findTweet!.likes.length >= 2) {
-    // //             console.log(`@${findTweet?.detailTweet().author} : ${findTweet?.content} `)
-    // //             console.log(`[ @${findTweet!.likes[0].authorlike.username} e mais ${findTweet?.likes.length - 1} curtiram ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-
-    // //         if (findTweet) {
-    // //             if (findTweet.replies.length) {
-    // //                 console.log(`@${findTweet.detailTweet().author} : ${findTweet.content}`)
-    // //                 console.log(`> @${findTweet.replies.find(item => item.authorReply.username)}: ${findTweet.replies.find(item => item.contentTweetReply)}\n likes: ${findTweet.replies.find(item => item.likes.length)}`)
-    // //                 console.log("=========================================================================")
-    // //             }
-    // //         }
-    // //     }
-
-
-    // //     if (findReply) {
-    // //         if (findReply.likes.length === 0) {
-    // //             console.log(`@${findReply.authorTweet.username} : ${findReply!.contentTweetReply}`)
-    // //             console.log(`[ ${findReply.likes.length} curtidas ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-
-    // //         else if (findReply.likes.length === 1) {
-    // //             console.log(`@${findReply.authorTweet.username} : ${findReply!.contentTweetReply}`)
-    // //             console.log(`[ @${findReply!.likes.map(item => item.authorlike.username)} curtiu isso ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-
-    // //         else if (findReply.likes.length >= 2) {
-    // //             console.log(`@${findReply.authorTweet.username} : ${findReply!.contentTweetReply} `)
-    // //             console.log(`[ @${findReply!.likes[0].authorlike.username} e outros ${findReply!.likes.length - 1} curtiram ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-    // //     }
-
-    // //     if (findReply) {
-    // //         for (const reply of replies) {
-    // //             const authorTweet = reply.authorReply.username
-    // //             const contentTweet = reply.detailReplie().replyTo?.content
-    // //             const authorReply = reply.authorTweet.username
-    // //             const content = reply.contentTweetReply
-    // //             const likes = reply.likes.length
-    // //             console.log(`@${authorTweet} : ${contentTweet}`)
-    // //             console.log(`> @${authorReply}: ${content}\n [ ${likes} likes ] \n [ ${replies.length - 1} replies ]`)
-    // //             console.log("=========================================================================")
-    // //         }
-    // //     }
-    // }
-
 }
 
 export default Tweet
