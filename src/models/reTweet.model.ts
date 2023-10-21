86504229814c5705f6b816bdfc31d30ea794dde6
Diego <dl7designer@gmail.com>
2023-10-21 15:00:51 -0300

    refatoracao da model retweet

--- src/models/reTweet.model.ts
+++ src/models/reTweet.model.ts
@@ -23,18 +23,6 @@ export class ReTweet {
         return this._idReTweet
     }
 
-    public includeLikeReTweet(tweet: Liker) {
-        this.likes.push(tweet)
-    }
-
-    public removeLikeReTweet(like: Liker) {
-        const index = this.likes.findIndex(item => item.idLike === like.idLike)
-        if (index !== -1) {
-            this.likes.splice(index, 1)
-        }
-    }
-
-
     public detailReTweet() {
         return {
             idTweetOriginal: this.idTweetOriginal,
