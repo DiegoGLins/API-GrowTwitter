97f01f2c7fbc78331c4e0adc3f48c9315b4cb46c
Diego <dl7designer@gmail.com>
2023-10-26 17:12:37 -0300

    alteracao na model de tweet

--- src/models/tweet.model.ts
+++ src/models/tweet.model.ts
@@ -1,4 +1,37 @@
-import { Liker } from "./liker.model";
+// import { TweetType } from "../types/TweetType";
+// class Tweet {
+//     public idUser: string;
+//     public content: string;
+//     public authorTweet: string;
+//     public idReTweet?: string
+//     public tipo: TweetType.normal | TweetType.reTweet
+
+//     constructor(private _id: string, content: string, idUser: string, authorTweet: string, idReTweet?: string) {
+//         this.idReTweet = idReTweet
+//         this.authorTweet = authorTweet
+//         this.content = content
+//         this.idUser = idUser;
+//         this.tipo = TweetType.normal || TweetType.reTweet
+//     }
+
+//     public get id() {
+//         return this._id;
+//     }
+
+//     detailTweet() {
+//         return {
+//             id: this._id,
+//             idReTweet: this.idReTweet,
+//             content: this.content,
+//             idUser: this.idUser,
+//             authorTweet: this.authorTweet,
+//             tipo: this.tipo
+//         }
+//     }
+// }
+
+// export default Tweet
+
 import { ReTweet } from "./reTweet.model";
 class Tweet {
     public idUser: string;
