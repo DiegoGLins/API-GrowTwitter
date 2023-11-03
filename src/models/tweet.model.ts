53698a6cf7eab07bf2a6873471f66c264f84a8f5
Diego <dl7designer@gmail.com>
2023-11-02 21:51:39 -0300

    alteracao no tweet model

--- src/models/tweet.model.ts
+++ src/models/tweet.model.ts
@@ -1,68 +1,52 @@
-// import { TweetType } from "../types/TweetType";
-// class Tweet {
-//     public idUser: string;
-//     public content: string;
-//     public authorTweet: string;
-//     public idReTweet?: string
-//     public tipo: TweetType.normal | TweetType.reTweet
+import { TweetType } from "../types/TweetType";
 
-//     constructor(private _id: string, content: string, idUser: string, authorTweet: string, idReTweet?: string) {
-//         this.idReTweet = idReTweet
-//         this.authorTweet = authorTweet
-//         this.content = content
-//         this.idUser = idUser;
-//         this.tipo = TweetType.normal || TweetType.reTweet
-//     }
-
-//     public get id() {
-//         return this._id;
-//     }
-
-//     detailTweet() {
-//         return {
-//             id: this._id,
-//             idReTweet: this.idReTweet,
-//             content: this.content,
-//             idUser: this.idUser,
-//             authorTweet: this.authorTweet,
-//             tipo: this.tipo
-//         }
-//     }
-// }
-
-// export default Tweet
-
-import { ReTweet } from "./reTweet.model";
 class Tweet {
     public idUser: string;
-    public reTweets: ReTweet[];
     public content: string;
     public authorTweet: string
+    public idTweetOriginal?: string;
+    public avatar: string;
+    public type: TweetType
+    //  id?: string,
+    //content: string,
+    //idUser: string,
+    //authorTweet: string,
+    //type: string;
+    //idTweetOriginal?: string
 
-    constructor(private _id: string, content: string, idUser: string, authorTweet: string) {
+    constructor(private _id: string, content: string, idUser: string, authorTweet: string, type: TweetType, idTweetOriginal?: string) {
+        this.avatar = ''
         this.authorTweet = authorTweet
         this.content = content
+        this.idTweetOriginal = idTweetOriginal
         this.idUser = idUser;
-        this.reTweets = [];
+        this.type = type
     }
+    // "idTweet": "221a641b-cb17-4777-850d-a4a80d601e8b",
+    //"content": "N",
+    //"type": "N",
+    //"idUser": "Essa nova rede social é muito massa",
+    //"authorTweet": "e5c2f752-290f-4e7f-b2c3-80b2b4278eef"
 
     public get id() {
         return this._id;
+    }
+
+    detailReTweet() {
+        return {
+            idTweet: this._id,
+            content: this.content,
+            idUser: this.idUser,
+            authorTweet: this.authorTweet,
+            idTweetOriginal: this.idTweetOriginal
+        }
     }
 
     detailTweet() {
         return {
             idTweet: this._id,
             content: this.content,
-            idUser: this.idUser,
-            authorTweet: this.authorTweet,
-        }
-    }
-
-    detailTweetCreate() {
-        return {
-            idTweet: this._id,
-            content: this.content,
+            type: this.type,
             idUser: this.idUser,
             authorTweet: this.authorTweet
         }
