eb2cf24735ad7d44a3bf6188e7510fa482b71062
Diego <dl7designer@gmail.com>
2023-11-02 21:52:29 -0300

    refatoracao das rotas de retweet ainda em revisao

--- src/routes/reTweets.routes.ts
+++ src/routes/reTweets.routes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express"
 import authMiddleware from "../middlewares/auth.middleware"
-import ReTweetController from "../controllers/reTweet.controller"
+import TweetController from "../controllers/tweet.controller"
 
 
 export const reTweetsRoutes = () => {
     const router = Router()
-    const controller = new ReTweetController()
+    const controller = new TweetController()
 
     router.post("/", authMiddleware, controller.createReTweet)
-
-    router.get("/", authMiddleware, controller.listReTweets)
-    router.put("/:idReTweet", authMiddleware, controller.updateReTweet)
-    router.delete("/:idReTweet", authMiddleware, controller.deleteReTweet)
+    // router.get("/", authMiddleware, controller.listReTweets)
+    // router.put("/:idReTweet", authMiddleware, controller.updateReTweet)
+    // router.delete("/:idReTweet", authMiddleware, controller.deleteReTweet)
 
     return router
 }
