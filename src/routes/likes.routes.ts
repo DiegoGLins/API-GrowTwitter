b335f2095050922b0ce31d41ca184338676087cd
Diego <dl7designer@gmail.com>
2023-11-19 11:58:07 -0300

    novos testes de rotas

--- src/routes/likes.routes.ts
+++ src/routes/likes.routes.ts
@@ -6,7 +6,7 @@ export const likesRoutes = () => {
     const router = Router()
     const controller = new LikerController()
 
-    router.get("/:idTweet", authMiddleware, controller.createLike)
+    router.post("/", authMiddleware, controller.createLike)
     router.get("/", authMiddleware, controller.listLike)
 
     return router
