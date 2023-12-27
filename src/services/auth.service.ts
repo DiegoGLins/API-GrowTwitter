30301829ea902d07ae1d7eb8714c748d40ed75cd
Diego <dl7designer@gmail.com>
2023-12-27 00:55:33 -0300

    inclusao de hash e jwt no authservice

---
+++ src/services/auth.service.ts
@@ -0,0 +1,53 @@
+import userService from "./user.service"
+import { compare } from "bcrypt";
+import jwt from 'jsonwebtoken'
+
+class AuthService {
+    public async login(username: string, password: string) {
+        const user = await userService.getByUser(username)
+
+        if (!user) {
+            return {
+                ok: false,
+                code: 404,
+                message: "usuario não encontrado"
+            }
+        }
+
+        const validatePassword = await compare(password, user.password)
+
+        if (!validatePassword) {
+            return {
+                ok: false,
+                code: 401,
+                message: "username ou senha incorretos"
+            }
+        }
+
+        const authHeader = {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            avatar: user.avatar,
+            email: user.email,
+        }
+
+        const token = jwt.sign(authHeader, `${process.env.SECRET_WORD}`, {
+            expiresIn: "8hr"
+        })
+        const update = await userService.updateUser({ ...user, token: token })
+        if (update.code === 200) {
+            return {
+                ok: true,
+                code: 200,
+                message: "Login realizado com sucesso",
+                data: {
+                    authHeader,
+                    token
+                }
+            }
+        }
+    }
+}
+
+export default new AuthService()
