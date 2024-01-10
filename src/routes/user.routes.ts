6536ee4306638fec1c8f4acc24013b4215c9b718
Diego <dl7designer@gmail.com>
2024-01-10 19:26:39 -0300

    alteracao no user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express"
 import { UserController } from "../controllers/user.controller"
-import validateUserMiddleware from "../middlewares/validate.middleware"
 import authMiddleware from "../middlewares/auth.middleware"
+import registerMiddleware from "../middlewares/register.middleware"
 
 export const userRoutes = () => {
     const router = Router()
     const controller = new UserController()
     router.get('/byId', authMiddleware, controller.listById)
-    router.post("/", validateUserMiddleware, controller.register)
+    router.post("/", registerMiddleware, controller.register)
 
     router.get("/", authMiddleware, controller.list)
     router.delete("/:id", authMiddleware, controller.delete)
