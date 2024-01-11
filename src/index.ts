cb6442101ec413d21ee31c3db88b6a2607cab61f
Diego <dl7designer@gmail.com>
2024-01-11 00:47:38 -0300

    config swagger

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,21 @@ import { tweetsRoutes } from "./routes/tweets.routes";
 import { likesRoutes } from "./routes/likes.routes";
 import { followRoutes } from "./routes/follow.routes";
 import { reTweetsRoutes } from "./routes/reTweets.routes";
+import swaggerUi from "swagger-ui-express"
+import swaggerDoc from "./docs/swagger.json"
 
 dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 app.use("/auth", authRoutes())
 app.use("/users", userRoutes())
 app.use("/tweets", tweetsRoutes())
 app.use("/reTweets", reTweetsRoutes())
 app.use("/likes", likesRoutes())
 app.use("/follows", followRoutes())
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 const port = process.env.PORT
 
