38bc225cdfbd439dd0ec76df45fec91f9f79ba52
Diego <dl7designer@gmail.com>
2024-01-11 23:31:44 -0300

    teste liker model criado

--- tests/model/liker.model.spec.ts
+++ tests/model/liker.model.spec.ts
@@ -12,4 +12,17 @@ describe('Liker Model', () => {
 
         expect(result).toBe("any_idLike")
     })
+
+    test.only('Deve retornar todas as informações do tweet curtido', () => {
+        const sut = createSut()
+
+        const result = sut.detailLike()
+
+        expect(result).toBeDefined()
+        expect(result).toHaveProperty("idLike", expect.any(String))
+        expect(result).toHaveProperty("idAuthorTweet", "any_idAuthorTweet")
+        expect(result).toHaveProperty("idAuthorLike", "any_idAuthorLike")
+        expect(result).toHaveProperty("authorLike", "any_authorLike")
+        expect(result).toHaveProperty("contentTweetLiked", "any_contentTweetLiked")
+    })
 })
