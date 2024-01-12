f1fea7b0e2826d8cdf7f21b9a687143399b55272
Diego <dl7designer@gmail.com>
2024-01-11 23:31:21 -0300

    config do prisma mock

---
+++ tests/config/prisma.mock.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
+import prisma from "../../src/database/prisma.database"
+
+jest.mock("../../src/database/prisma.database", () => ({
+    __esModule: true,
+    default: mockDeep<PrismaClient>(),
+}));
+
+beforeEach(() => {
+    console.log("executou o mock reset - prisma mock");
+    mockReset(prismaMock);
+});
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
