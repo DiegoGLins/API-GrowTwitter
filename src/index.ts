29be7dee987570e169dc988b331320e5d1d7087a
Diego <dl7designer@gmail.com>
2024-01-15 22:41:08 -0300

    atualizacao do index

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,14 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
 import * as dotenv from "dotenv";
-import { userRoutes } from "./routes/user.routes";
-import { authRoutes } from "./routes/auth.routes";
-import { tweetsRoutes } from "./routes/tweets.routes";
-import { likesRoutes } from "./routes/likes.routes";
-import { followRoutes } from "./routes/follow.routes";
-import { reTweetsRoutes } from "./routes/reTweets.routes";
+
 import swaggerUi from "swagger-ui-express"
 import swaggerDoc from "./docs/swagger.json"
+import { createServer } from './express.server'
 
 dotenv.config()
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.use("/auth", authRoutes())
-app.use("/users", userRoutes())
-app.use("/tweets", tweetsRoutes())
-app.use("/reTweets", reTweetsRoutes())
-app.use("/likes", likesRoutes())
-app.use("/follows", followRoutes())
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
 const port = process.env.PORT
+const app = createServer()
 
-app.get("/", (req: Request, res: Response) => {
-    return res.status(200).send({ sucess: true, mesage: "API - GrowTwiter" })
-});
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.listen(port, () => {
     console.log(`API está rodando na porta ${port} `);
 });
