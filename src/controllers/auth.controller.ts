6bd33f1b3cf065ad05b1d5b166887e49eca11850
Diego <dl7designer@gmail.com>
2024-01-18 21:13:48 -0300

    alteracao no auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,19 +5,8 @@ class AuthController {
         try {
             const { username, password } = req.body
 
-            if (!username || !password) {
-                return res.status(400).send({
-                    ok: false,
-                    code: 400,
-                    message: "Campos não informados"
-                })
-            }
-
             const result = await authService.login(username, password)
-
-            if (result?.code === 200) {
-                return res.status(result.code).send(result.data)
-            }
+            return res.status(result.code).send(result)
         }
 
         catch (error: any) {
@@ -28,42 +17,6 @@ class AuthController {
             })
         }
     }
-
-    // public async logout(req: Request, res: Response) {
-    //     try {
-    //         const { idUser } = req.body
-
-    //         if (!idUser) {
-    //             return res.status(401).send({
-    //                 ok: false,
-    //                 code: 401,
-    //                 message: "Autenticação do token falhou",
-    //             });
-    //         }
-
-    //         const userLogged = await userService.getById(idUser)
-
-    //         if (userLogged) {
-    //             const logged = userService.mapToModel(userLogged.data).detailUser()
-
-    //             return res.status(200).send({
-    //                 ok: true,
-    //                 code: 200,
-    //                 message: "Logout realizado com sucesso",
-    //                 data: {
-    //                     logged
-    //                 }
-    //             })
-    //         }
-
-    //     } catch (error: any) {
-    //         res.status(500).send({
-    //             ok: false,
-    //             code: 500,
-    //             message: "Erro interno do servidor"
-    //         })
-    //     }
-    // }
 }
 
 export default AuthController
