b3297192995794bd072e4b5199cb02d27de67eca
Diego <dl7designer@gmail.com>
2024-01-19 00:18:30 -0300

    add testes unit follow.service

--- tests/services/follow.service.spec.ts
+++ tests/services/follow.service.spec.ts
@@ -73,5 +73,90 @@ describe('FollowService', () => {
                 message: "Você não pode seguir a si mesmo"
             })
         })
+
+        test('Deve retornar "Você já está seguindo o usuário nome_do_usuario_seguido" se já estiver seguindo o usuário', async () => {
+            const sut = createSut();
+
+            prismaMock.user.findUnique.mockResolvedValue({
+                id: "any_id",
+                avatar: "any_avatar",
+                name: "any_name",
+                username: "any_username",
+                email: "any_email",
+                password: "any_password"
+            });
+
+            prismaMock.follow.findFirst.mockResolvedValue({
+                id: "any_follow_id",
+                idUserFollowing: "any_idUserFollowing",
+                usernameFollowing: "any_usernameFollowing",
+                idUserFollower: "other_id",
+                usernameFollower: "other_username",
+            });
+
+            const result = await sut.FollowService.addFollowing({
+                idUserFollowing: "any_idUserFollowing",
+                usernameFollowing: "any_usernameFollowing",
+                idUserFollower: "other_id",
+                usernameFollower: "other_username",
+            });
+
+            expect(result).toEqual({
+                ok: false,
+                code: 400,
+                message: `Você já está seguindo o usuário ${"any_usernameFollowing"}`,
+            });
+        });
+
     })
+    describe('deleteFollow', () => {
+        test('Deve retornar "Usuario não encontrado" se o usuário não estiver sendo seguido', async () => {
+            const sut = createSut();
+
+            prismaMock.follow.findUnique.mockResolvedValue(null);
+
+            const result = await sut.FollowService.deleteFollow('any_username', 'any_id');
+
+            expect(result).toEqual({
+                ok: false,
+                code: 404,
+                message: 'Usuario não encontrado',
+            });
+        });
+
+        test('Deve retornar "Voce deixou de seguir nome_do_usuario_seguido" e os dados do usuário seguido se o usuário estiver sendo seguido', async () => {
+            const sut = createSut();
+
+            prismaMock.follow.findUnique.mockResolvedValue({
+                id: "any_follow_id",
+                idUserFollowing: "any_idUserFollowing",
+                usernameFollowing: "any_usernameFollowing",
+                idUserFollower: "other_id",
+                usernameFollower: "other_username",
+            });
+
+            prismaMock.follow.delete.mockResolvedValue({
+                id: "any_follow_id",
+                idUserFollowing: "any_idUserFollowing",
+                usernameFollowing: "any_usernameFollowing",
+                idUserFollower: "other_id",
+                usernameFollower: "other_username",
+            });
+
+            const result = await sut.FollowService.deleteFollow('any_usernameFollowing', 'any_idUserFollowing');
+
+            expect(result).toEqual({
+                ok: true,
+                code: 200,
+                message: `Voce deixou de seguir ${"any_usernameFollowing"}`,
+                data: {
+                    id: "any_follow_id",
+                    idUserFollowing: "any_idUserFollowing",
+                    usernameFollowing: "any_usernameFollowing",
+                    idUserFollower: "other_id",
+                    usernameFollower: "other_username",
+                },
+            });
+        });
+    });
 })
