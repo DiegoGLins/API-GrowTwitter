730f1d8fd0d3879bab61095d502e11ec1e814a1a
Diego <dl7designer@gmail.com>
2024-01-19 00:19:20 -0300

    removida rota logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 import validateUserMiddleware from "../middlewares/validate.middleware";
 import AuthController from '../controllers/auth.controller';
-import authMiddleware from '../middlewares/auth.middleware';
 
 export const authRoutes = () => {
     const router = Router()
     const controller = new AuthController()
 
     router.post("/login", validateUserMiddleware, controller.login)
-    // router.get("/logout", authMiddleware, controller.logout)
     return router
 }
